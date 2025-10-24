358e9b3fdfbd2a2e526b1ad1eab4ab202449aaa1
andi49 <46962871+andi49@users.noreply.github.com>
2025-10-24 09:51:03 +0200

    24 oktober

---
+++ JavaScriptSkola/24oktober/assets/loops.js
@@ -0,0 +1,16 @@
+
+
+//5 olika datatyper
+let stringVariabel = "Some text";
+let numberVariabel = 0.826;
+let booleanVariabel = true;
+let h1Variabel = document.querySelector('h1');
+let undefinedVariabel;
+
+
+//för att ta reda på vilken datatyp en variabel har kan man använda keyword typeof
+console.log(typeof stringVariabel);
+console.log(typeof numberVariabel);
+console.log(typeof booleanVariabel);
+console.log(typeof h1Variabel);
+console.log(typeof undefinedVariabel);
