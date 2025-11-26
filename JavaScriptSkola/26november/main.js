505cb99526b581a316d6a56b3866fbe38f0e416c
andi49 <46962871+andi49@users.noreply.github.com>
2025-11-26 11:26:45 +0100

    Update

--- JavaScriptSkola/26november/main.js
+++ JavaScriptSkola/26november/main.js
@@ -1,1 +1,40 @@
 /// Async med Fetch & Promises - Agenda
+const url = 'https://dog.ceo/api/breeds/image/random';
+
+// const requestPromise = fetch(url)
+
+// console.log(requestPromise)
+
+// requestPromise.then(
+//     function(responseOBJ) {
+//         console.log(responseOBJ)
+//         // console.log(responseOBJ.json())
+//         return responseOBJ
+//     }
+// )
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', handleSubmit );
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    const breed = document.querySelector('input').value;
+    console.log(breed);
+    form.reset();
+
+    const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+    // console.log(url);
+    fetch(url)
+        .then( response => response.json() )
+        .then( displayDogImage );
+}
+
+function displayDogImage(dogObj){
+    console.log(dogObj);
+
+    const img = document.querySelector('#dogImage');
+    document.body.append(img);
+    img.src = dogObj.message;
+}
