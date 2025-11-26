52d61a4457e1e218f08db707ccfa27a8322a94d6
andi49 <46962871+andi49@users.noreply.github.com>
2025-11-26 11:51:26 +0100

    uop

--- JavaScriptSkola/26november/main.js
+++ JavaScriptSkola/26november/main.js
@@ -27,14 +27,32 @@ function handleSubmit(event){
     const url = `https://dog.ceo/api/breed/${breed}/images/random`;
     // console.log(url);
     fetch(url)
-        .then( response => response.json() )
-        .then( displayDogImage );
+        .then( handleResponse )
+        .then( displayDogImage )
+        .catch(handleError)
+}
+
+function handleResponse(response){
+    console.log(response);
+    if(response.status >= 200 && response.status < 300){
+        return response.json();
+    }
+    else if(response.status >= 400 && response.status < 500){
+        throw 'The breed does not exist. Please check your spelling.';
+    }
+    else if(!response.ok){
+        throw 'Something went wrong. Please try again later.';
+    }
 }
 
 function displayDogImage(dogObj){
-    console.log(dogObj);
+    // console.log(dogObj);
 
     const img = document.querySelector('#dogImage');
     document.body.append(img);
     img.src = dogObj.message;
+}
+
+function handleError(error) {
+    console.log(error)
 }
