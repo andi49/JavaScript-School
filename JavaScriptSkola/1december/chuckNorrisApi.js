05e6cfc7a7bcd0ad392a68beea5e20ff1176100f
andi49 <46962871+andi49@users.noreply.github.com>
2025-12-01 10:42:40 +0100

    summ update

--- JavaScriptSkola/1december/chuckNorrisApi.js
+++ JavaScriptSkola/1december/chuckNorrisApi.js
@@ -1,17 +1,39 @@
-async function getCatergories() 
- {
-   const URL = 'https://api.chucknorris.io/jokes/categories';
+async function getCatergories() {
+  const URL = "https://api.chucknorris.io/jokes/categories";
 
-   const response = await fetch(URL)
-  
-
-   const data = await response.json()
-    return data
+  try {
+    const response = await fetch(URL);
+    console.log(response)
+    if(!response.ok) {
+        throw 'Something went wrong'
+    } 
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error)
+    throw error;
+  }
 }
 
-function displayCatergories() {
+function displayCatergories(arr) {
+  const select = document.querySelector("#category");
 
+  for (const category of arr) {
+    const option = document.createElement("option");
+    option.innerText = category;
+    console.log(category);
+
+    option.innerText = category;
+    option.value = category;
+    select.append(option);
+  }
+}
+
+function displayError () {
+    const h1Error = document.querySelector('#error')
+    h1Error.innerText = error
 }
 
 getCatergories()
 .then(displayCatergories)
+.catch(displayError)
