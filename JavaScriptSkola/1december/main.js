5915072c9244b6562e08bb2098718dbcc4d1ee24
andi49 <46962871+andi49@users.noreply.github.com>
2025-12-01 14:14:37 +0100

    update

--- JavaScriptSkola/1december/main.js
+++ JavaScriptSkola/1december/main.js
@@ -1,1 +1,38 @@
 ///// Objects & Methods - Agenda
+
+
+/// STRING 
+
+const string1 = 'Den spanska räven'
+
+console.log(string1.length) /// Engenskapen på stringen
+
+const string2 = 'rev en annan räv'
+const string3 = string1.concat(' ', string2) // Metod som lägger ihop string1 och argumeent till en ny sting
+
+console.log(string3)
+const fakeh1 = document.querySelector('#strings')
+fakeh1.innerHTML = string3
+/// STRING END
+
+
+/// ARRAY
+
+const lander = ['Sweden', 'Norway', 'Findland']
+
+console.log(lander.length)
+
+lander.push('Island')
+const fakeh2 = document.querySelector('#landers')
+fakeh2.innerHTML = lander
+
+/// ARRAY END
+
+/// MATH
+
+console.log(Math.PI) /// 
+
+console.log(Math.round(6.78))
+
+/// MATH END
+
