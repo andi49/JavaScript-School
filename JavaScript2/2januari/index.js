daef1bacab2a789a3f2983d4f9bffcd86d773af0
andi49 <46962871+andi49@users.noreply.github.com>
2026-01-02 10:16:35 +0100

    New day new maps

---
+++ JavaScript2/2januari/index.js
@@ -0,0 +1,19 @@
+// PROTOTYPE
+// För info om hur prototype finns överallt i js 
+// console.log(document)
+// console.log(Object.getPrototypeOf(document))
+
+// const obj = {
+//     p:1 
+// }
+
+// console.log(obj)
+
+// function test(){}
+
+// console.log(test)
+
+
+function Animal(type, soun) {
+    
+}
