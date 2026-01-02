4f14f935b8e9b8240e7c882c2d982d055fa86a51
andi49 <46962871+andi49@users.noreply.github.com>
2026-01-02 11:26:47 +0100

    Coolt

---
+++ JavaScript2/2januari/main.js
@@ -0,0 +1,28 @@
+import {CountryCard} from './country.js'
+
+// const country = new CountryCard('Sweden', 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Sweden.svg')
+
+// console.log(country)
+
+// country.createCard(document.body)
+
+async function getCountries(language){
+    const url = `https://restcountries.com/v3.1/lang/${language}`;
+
+    const response = await fetch(url);
+    const countries = await response.json();
+
+    return countries;
+}
+
+
+getCountries('russian')
+    .then( countries =>{
+        console.log(countries);
+
+        const countryCards = countries.map( ({name, flags}) => new CountryCard(name.common, flags.png) )
+
+        console.log(countryCards);
+
+        countryCards.forEach( card => card.displayCard(document.body))
+    })
