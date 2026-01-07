581af776c1446b382ecc7349c45abb22f737e695
andi49 <46962871+andi49@users.noreply.github.com>
2026-01-07 11:34:26 +0100

    Working bank

--- JavaScript2/7januari/main.js
+++ JavaScript2/7januari/main.js
@@ -1,5 +1,5 @@
-import { Account } from "encrypted.js"
-
+import { Account } from "./account.js";
+import { AccountGUI } from "./accountGUI.js";
 
 const account = new Account('savings', 0);
 
@@ -10,8 +10,19 @@ console.log(account.getName())
 
 // account.generateID(); //ger error
 
-account.nyEgenskap = 'lalalal'
-console.log(account)
+// account.nyEgenskap = 'lalalal' //ger error eftersom vi returnerar a sealed object i construktorn
+// console.log(account)
 
-const account2 = new Account('sdfs', 0);
+const account2 = new Account('Test', 10000);
 console.log(account2)
+
+const aGUI = new AccountGUI('Car', 3000);
+console.log(aGUI)
+
+aGUI.renderAccount(document.body)
+
+const toggle = document.querySelector('button');
+toggle.addEventListener('click', event =>{
+    aGUI.toggleDarkMode();
+    console.log(aGUI)
+})
