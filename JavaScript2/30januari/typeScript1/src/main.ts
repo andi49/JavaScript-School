23834b589bf75be778b7201054ff11e21f328435
andi49 <46962871+andi49@users.noreply.github.com>
2026-01-30 11:48:07 +0100

    TypeScript

--- JavaScript2/30januari/typeScript1/src/main.ts
+++ JavaScript2/30januari/typeScript1/src/main.ts
@@ -1,24 +1,66 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// Declare a variabel in TS   
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let num = 13 
+// num = 'test' // får inte göra så här!!! :(((())))
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+console.log(num)  /// det är fel 
+
+let isSnowing: boolean
+
+isSnowing = true
+
+console.log(isSnowing)
+
+const arr = [1,2,3,4,5, 'test']
+arr.push('test') // man får inte göra så här, måste vara nummber eller om det finns en string kommer det funka
+
+const newArr = [...arr]
+console.log(newArr)
+
+// Funktioner 
+
+const add = (x:number, y:number) => {
+  return x + y
+}
+
+console.log(add(6,7))
+
+const logName = (first:string, last:string) => {
+console.log(first, last)
+}
+
+logName('Andi', 'OnlyFans')
+
+const multi = (x:number, y:number = 10) => {
+  return x + y
+}
+
+console.log(multi(1))
+console.log(multi(1,9))
+
+const callback = (a:string) => {
+  console.log(a)
+}
+
+const ho = (func:Function) => {
+  func('hej')
+}
+ho(callback)
+
+// union types
+
+// const coinFlip = ():'heads' | 'tails' => {
+//   return Math.random() > 0.5 ? 'heads' : 'tails'
+// }
+
+// console.log(coinFlip())
+
+/// type alias
+
+type Coin = 'heads' | 'tails'
+
+const coinFlip = ():Coin => {
+  return Math.random() > 0.5 ? 'heads' : 'tails'
+}
+
+console.log(coinFlip())
