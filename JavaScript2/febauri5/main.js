beb34105e9c50ad9a03b4914398a40565205be57
andi49 <46962871+andi49@users.noreply.github.com>
2026-02-06 10:15:45 +0100

    Test

--- JavaScript2/febauri5/main.js
+++ JavaScript2/febauri5/main.js
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+console.log('Hello World again!')
+
+const greetings = 'Peace is a lie'
+console.log(greetings)
+
+const arr = [2,4,6,8,10]
+arr.forEach(num => console.log(num))
